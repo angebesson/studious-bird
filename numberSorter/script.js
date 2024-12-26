28a3d7b81143099d5836097dda137b77f7e7d7ee
Angelina Besson <formadeltiempoange@gmail.com>
2024-12-26 12:45:45 -0300

    finishing with sort function

--- numberSorter/script.js
+++ numberSorter/script.js
@@ -3,7 +3,9 @@ const sortInputArray = (event) => {
     event.preventDefault();
 
     const inputValues = [...document.getElementsByClassName("values-dropdown")].map(dropdown => Number(dropdown.value))
-    const sortedValues = inputValues.sort();
+    const sortedValues = inputValues.sort((a,b)=>{
+      return a-b
+    });
 
     updateUI(sortedValues);
 }
