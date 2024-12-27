2884f8cebd59fd3aad6ed62b3f89097805c8f1cd
Angelina Besson <formadeltiempoange@gmail.com>
2024-12-27 17:15:59 -0300

    get median function

--- statisticsCalculator/script.js
+++ statisticsCalculator/script.js
@@ -1,9 +1,19 @@
-const getMean = (array)=> array.reduce((acc, el) => acc + el, 0)/ array.length;
-
+const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length;
+const getMedian = (array) => {
+    const sorted = array.toSorted((a, b) => a - b);
+    if (sorted.length % 2 === 0) {
+        const mean = getMean([sorted[sorted.length / 2], sorted[(sorted.length / 2) - 1]]);
+        return mean;
+    }
+    else {
+        const middle = getMean([sorted[Math.floor(sorted.length / 2)]]);
+        return middle;
+    }
+};
 const calculate = () => {
     const value = document.querySelector("#numbers").value;
     const array = value.split(/,\s*/g);
     const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
     const mean = getMean(numbers);
     document.querySelector("#mean").textContent = mean
-  }
+}
