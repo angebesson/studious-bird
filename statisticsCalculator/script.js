3562b3814d1b71bf7519db0a16e976037017e0a5
Angelina Besson <formadeltiempoange@gmail.com>
2024-12-28 11:31:28 -0300

    getRange function

--- statisticsCalculator/script.js
+++ statisticsCalculator/script.js
@@ -28,25 +28,29 @@ const getMedian = (array) => {
 const getMode = (array) => {
     const counts = {};
     array.forEach((el) => {
-      counts[el] = (counts[el] || 0) + 1;
+        counts[el] = (counts[el] || 0) + 1;
     })
     if (new Set(Object.values(counts)).size === 1) {
-      return null;
+        return null;
     }
-    const highest = Object.keys(counts).sort((a,b)=>{return counts[b]-counts[a]})[0];
-    const mode = Object.keys(counts).filter(el=>counts[el]===counts[highest]);
+    const highest = Object.keys(counts).sort((a, b) => { return counts[b] - counts[a] })[0];
+    const mode = Object.keys(counts).filter(el => counts[el] === counts[highest]);
     return mode.join(", ")
-  }
+}
+
+const getRange = (array) => { return (Math.max(...array)) - (Math.min(...array)) }
+
 const calculate = () => {
     const value = document.querySelector("#numbers").value;
     const array = value.split(/,\s*/g);
     const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
     const mean = getMean(numbers);
-  const median = getMedian(numbers);
-  const mode = getMode(numbers);
+    const median = getMedian(numbers);
+    const mode = getMode(numbers);
+    const range = getRange(numbers);
 
-
-  document.querySelector("#mean").textContent = mean;
-  document.querySelector("#median").textContent = median;
-  document.querySelector("#mode").textContent = mode;
+    document.querySelector("#mean").textContent = mean;
+    document.querySelector("#median").textContent = median;
+    document.querySelector("#mode").textContent = mode;
+    document.querySelector("#range").textContent = range;
 }
