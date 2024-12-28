b625665440651a5d29f8b55c22ab7e09aa28b56a
Angelina Besson <formadeltiempoange@gmail.com>
2024-12-28 11:24:37 -0300

    getMode finished and added to calculate

--- statisticsCalculator/script.js
+++ statisticsCalculator/script.js
@@ -34,6 +34,8 @@ const getMode = (array) => {
       return null;
     }
     const highest = Object.keys(counts).sort((a,b)=>{return counts[b]-counts[a]})[0];
+    const mode = Object.keys(counts).filter(el=>counts[el]===counts[highest]);
+    return mode.join(", ")
   }
 const calculate = () => {
     const value = document.querySelector("#numbers").value;
@@ -41,7 +43,10 @@ const calculate = () => {
     const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
     const mean = getMean(numbers);
   const median = getMedian(numbers);
+  const mode = getMode(numbers);
+
 
   document.querySelector("#mean").textContent = mean;
   document.querySelector("#median").textContent = median;
+  document.querySelector("#mode").textContent = mode;
 }
