fd438f1a900e942c0d5a0b3c262808be815abae1
Angelina Besson <formadeltiempoange@gmail.com>
2025-01-03 13:04:43 -0300

    evalFormula currying

--- spreadSheet/script.js
+++ spreadSheet/script.js
@@ -10,8 +10,23 @@ const sum = (nums)=>nums.reduce( (accumulator, currentValue) => accumulator + cu
   const middle = length / 2 -1;
   return isEven(length) ? average([sorted[Math.round(middle)], sorted[Math.round(middle) + 1]]) : sorted[Math.ceil(middle)];
   }
+  const spreadsheetFunctions = {
+    sum,
+    average,
+    median,
+  }
+
+
+
 const range = (start, end) => Array(end - start + 1).fill(start).map((element, index) => element + index);
 const charRange = (start, end) => range(start.charCodeAt(0), end.charCodeAt(0)).map((code) => String.fromCharCode(code));
+const evalFormula = (x,cells)=>{
+    const idToText = (id)=>cells.find((cell)=>cell.id === id).value;
+    const rangeRegex = /([A-J])([1-9][0-9]?):([A-J])([1-9][0-9]?)/gi;
+    const rangeFromString = (num1,num2)=>range(parseInt(num1),parseInt(num2));
+    const elemValue = num => character => idToText(character + num);
+      const addCharacters = character1 => character2 => num => charRange(character1,character2);
+};
 window.onload = () => {
     const container = document.getElementById("container");
     const createLabel = (name) => {
@@ -29,7 +44,13 @@ window.onload = () => {
              input.type = "text";
              input.id = letter + number;
              input.ariaLabel = letter + number;
-             container.appendChild(input)
+             input.onchange = update;
+             container.appendChild(input);
           });
       });
+}
+const update = (event)=>{
+    const element = event.target;
+    const value = element.value.replace(/\s/g, '');
+    if(!value.includes(element.id)&& value[0] === "="){}
 }
