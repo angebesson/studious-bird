5ae265b6c3e8662c74f94cf1405697da4d6b106e
Angelina Besson <formadeltiempoange@gmail.com>
2025-01-06 17:45:12 -0300

    apply function

--- spreadSheet/script.js
+++ spreadSheet/script.js
@@ -8,8 +8,8 @@ const infixToFunction = {
 const infixEval = (str, regex) => str.replace(regex, (_match, arg1, operator, arg2) => infixToFunction[operator](parseFloat(arg1), parseFloat(arg2)));
 
 const highPrecedence = str => {
-  const regex =  /^\d+[*/]\d+$/;
-  const str2 = infixEval(str,regex);
+  const regex = /^\d+[*/]\d+$/;
+  const str2 = infixEval(str, regex);
   return str2 === str ? str : highPrecedence(str2);
 };
 
@@ -31,7 +31,15 @@ const spreadsheetFunctions = {
   median,
 };
 
-
+const applyFunction = (str) => {
+  const noHigh = highPrecedence(str);
+  const infix = /([\d.]+)([-+])([\d.]+)/;
+  const str2 = infixEval(noHigh, infix);
+  const functionCall = /([a-z0-9]*)\(([0-9., ]*)\)(?!.*\()/i;
+  const toNumberList = (args) => args.split(",").map(parseFloat);
+  const apply = (fn, args) => spreadsheetFunctions[fn.toLowerCase()](toNumberList(args));
+  return str2.replace(functionCall, (match, fn, args) => spreadsheetFunctions.hasOwnProperty(fn.toLowerCase()) ? apply(fn, args) : match)
+};
 
 
 const range = (start, end) => Array(end - start + 1).fill(start).map((element, index) => element + index);
@@ -42,7 +50,7 @@ const evalFormula = (x, cells) => {
   const rangeFromString = (num1, num2) => range(parseInt(num1), parseInt(num2));
   const elemValue = num => character => idToText(character + num);
   const addCharacters = character1 => character2 => num => charRange(character1, character2).map(elemValue(num));
-  const rangeExpanded = x.replace(rangeRegex,(_match,char1,num1,char2,num2)=>rangeFromString(num1,num2).map(addCharacters(char1)(char2)));
+  const rangeExpanded = x.replace(rangeRegex, (_match, char1, num1, char2, num2) => rangeFromString(num1, num2).map(addCharacters(char1)(char2)));
   const cellRegex = /[A-J][1-9][0-9]?/gi;
   const cellExpanded = rangeExpanded.replace(cellRegex, match => idToText(match.toUpperCase()));
 };
