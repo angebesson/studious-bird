8ad457f076039ce17a88a173499285ec89b98d78
Angelina Besson <formadeltiempoange@gmail.com>
2025-02-22 18:50:05 -0300

    in animate funciont adding if

--- platformerGame/script.js
+++ platformerGame/script.js
@@ -53,6 +53,32 @@ class Player {
     }
 }
 const player = new Player() ;
+
+const animate = () => {
+    if(keys.rightKey.pressed && player.position.x < proportionalSize(400)){
+        player.velocity.x = 5;
+    }else if(keys.leftKey.pressed && player.position.x > proportionalSize(100)){
+        player.velocity.x = -5;
+     }else{
+        player.velocity.x = 0;
+      }
+    requestAnimationFrame(animate);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    player.update();
+  }
+
+  const keys = {
+    rightKey: {
+           pressed: false
+       },
+       leftKey: {
+           pressed: false
+       },
+   };
+   const movePlayer = (key, xVelocity, isPressed)=>{
+    
+   };
+
 const startGame = () => {
     canvas.style.display = "block";
     startScreen.style.display = "none";
