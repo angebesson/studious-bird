5993c0f32f91a10fbff27747d16d5fbedabdf89f
Angelina Besson <formadeltiempoange@gmail.com>
2025-02-22 19:47:48 -0300

    move player function

--- platformerGame/script.js
+++ platformerGame/script.js
@@ -75,9 +75,33 @@ const animate = () => {
            pressed: false
        },
    };
-   const movePlayer = (key, xVelocity, isPressed)=>{
-    
-   };
+   const movePlayer = (key, xVelocity, isPressed) => {
+    if(!isCheckpointCollisionDetectionActive){
+        player.velocity.x = 0;
+        player.velocity.y = 0;
+        return;
+    }
+    switch(key){
+        case "ArrowLeft":
+            keys.leftKey.pressed = isPressed;
+            if (xVelocity === 0) {
+              player.velocity.x = xVelocity;
+      }
+      player.velocity.x -= xVelocity;
+      break;
+      case "ArrowUp":
+  case " ":
+  case "Spacebar":
+  
+    player.velocity.y -= 8;
+    break;
+    case "ArrowRight":
+     keys.rightKey.pressed = isPressed;
+      if (xVelocity === 0) {
+        player.velocity.x = xVelocity;
+      }
+      player.velocity.x += xVelocity;
+  }
 
 const startGame = () => {
     canvas.style.display = "block";
