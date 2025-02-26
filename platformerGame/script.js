0948156213a7a75f8f8a2b2d8b9c7d3736ad7f3a
Angelina Besson <formadeltiempoange@gmail.com>
2025-02-26 17:20:44 -0300

    platform positions

--- platformerGame/script.js
+++ platformerGame/script.js
@@ -53,79 +53,91 @@ class Player {
     }
 }
 class Platform {
-    constructor(x,y){
+    constructor(x, y) {
         this.position = {
-            x,y
-          };
+            x, y
+        };
         this.width = 200;
         this.height = proportionalSize(40);
     };
     draw() {
         ctx.fillStyle = "#acd157";
         ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
-      }
+    }
 };
-const player = new Player() ;
-
+const player = new Player();
+const platformPositions = [
+    { x: 500, y: proportionalSize(450) },
+    { x: 700, y: proportionalSize(400) },
+    { x: 850, y: proportionalSize(350) },
+    { x: 900, y: proportionalSize(350) },
+    { x: 1050, y: proportionalSize(150) },
+    { x: 2500, y: proportionalSize(450) },
+    { x: 2900, y: proportionalSize(400) },
+    { x: 3150, y: proportionalSize(350) },
+    { x: 3900, y: proportionalSize(450) },
+    { x: 4200, y: proportionalSize(400) },
+    { x: 4400, y: proportionalSize(200) },
+    { x: 4700, y: proportionalSize(150) },
+];
 const animate = () => {
-    if(keys.rightKey.pressed && player.position.x < proportionalSize(400)){
+    if (keys.rightKey.pressed && player.position.x < proportionalSize(400)) {
         player.velocity.x = 5;
-    }else if(keys.leftKey.pressed && player.position.x > proportionalSize(100)){
+    } else if (keys.leftKey.pressed && player.position.x > proportionalSize(100)) {
         player.velocity.x = -5;
-     }else{
+    } else {
         player.velocity.x = 0;
-      }
+    }
     requestAnimationFrame(animate);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     player.update();
-  }
+}
 
-  const keys = {
+const keys = {
     rightKey: {
-           pressed: false
-       },
-       leftKey: {
-           pressed: false
-       },
-   };
-   const movePlayer = (key, xVelocity, isPressed) => {
-    if(!isCheckpointCollisionDetectionActive){
+        pressed: false
+    },
+    leftKey: {
+        pressed: false
+    },
+};
+const movePlayer = (key, xVelocity, isPressed) => {
+    if (!isCheckpointCollisionDetectionActive) {
         player.velocity.x = 0;
         player.velocity.y = 0;
         return;
     }
-    switch(key){
+    switch (key) {
         case "ArrowLeft":
             keys.leftKey.pressed = isPressed;
             if (xVelocity === 0) {
-              player.velocity.x = xVelocity;
-      }
-      player.velocity.x -= xVelocity;
-      break;
-      case "ArrowUp":
-  case " ":
-  case "Spacebar":
-  
-    player.velocity.y -= 8;
-    break;
-    case "ArrowRight":
-     keys.rightKey.pressed = isPressed;
-      if (xVelocity === 0) {
-        player.velocity.x = xVelocity;
-      }
-      player.velocity.x += xVelocity;
-  }
-   }
+                player.velocity.x = xVelocity;
+            }
+            player.velocity.x -= xVelocity;
+            break;
+        case "ArrowUp":
+        case " ":
+        case "Spacebar":
+
+            player.velocity.y -= 8;
+            break;
+        case "ArrowRight":
+            keys.rightKey.pressed = isPressed;
+            if (xVelocity === 0) {
+                player.velocity.x = xVelocity;
+            }
+            player.velocity.x += xVelocity;
+    }
+}
 const startGame = () => {
     canvas.style.display = "block";
     startScreen.style.display = "none";
     player.draw();
-  }
-  startBtn.addEventListener("click", startGame);
-  window.addEventListener("keydown",({key})=>{
-    movePlayer(key,8,true)
-  });
-  window.addEventListener("keyup", ({ key }) => {
+}
+startBtn.addEventListener("click", startGame);
+window.addEventListener("keydown", ({ key }) => {
+    movePlayer(key, 8, true)
+});
+window.addEventListener("keyup", ({ key }) => {
     movePlayer(key, 0, false);
-  });
-  
+});
