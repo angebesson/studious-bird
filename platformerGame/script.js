6aa05be4273bdc8a72e7ac98eaba86a609a574df
Angelina Besson <formadeltiempoange@gmail.com>
2025-03-03 21:14:19 -0300

    platform detection rules

--- platformerGame/script.js
+++ platformerGame/script.js
@@ -100,13 +100,23 @@ const animate = () => {
         platforms.forEach((platform) => {
             const collisionDetectionRules = [player.position.y + player.height <= platform.position.y,
             player.position.y + player.height + player.velocity.y >= platform.position.y,
-            player.position.x >= platform.position.x - player.width/2,
-            player.position.x <= platform.position.x + platform.width - player.width/3,
+            player.position.x >= platform.position.x - player.width / 2,
+            player.position.x <= platform.position.x + platform.width - player.width / 3,
             ];
-            if(collisionDetectionRules.every((rule) => rule)){
+            if (collisionDetectionRules.every((rule) => rule)) {
                 player.velocity.y = 0;
                 return;
-            }
+            };
+            const platformDetectionRules = [
+                player.position.x >= platform.position.x - player.width / 2,
+                player.position.x <= platform.position.x + platform.width - player.width / 3,
+                player.position.y + player.height >= platform.position.y,
+                player.position.y <= platform.position.y + platform.height,
+            ];
+            if (platformDetectionRules.every(rule => rule)) {
+                player.position.y = platform.position.y + player.height;
+                player.velocity.y = gravity;
+              };         
         })
         requestAnimationFrame(animate);
         ctx.clearRect(0, 0, canvas.width, canvas.height);
